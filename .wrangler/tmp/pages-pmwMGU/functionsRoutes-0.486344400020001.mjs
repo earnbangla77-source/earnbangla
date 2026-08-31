f036eb017fe053bd13fd3bbaa8cadd9ea2103ca6
earnbangla77-source <Earnbangla77@gmail.com>
2026-08-31 10:49:52 +0600

    add

---
+++ .wrangler/tmp/pages-pmwMGU/functionsRoutes-0.486344400020001.mjs
@@ -0,0 +1,315 @@
+import { onRequestGet as __api_admin_dashboard_stats_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\admin\\dashboard-stats.js"
+import { onRequestGet as __api_admin_users_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\admin\\users.js"
+import { onRequestGet as __api_admin_withdrawals_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\admin\\withdrawals.js"
+import { onRequestPost as __api_admin_withdrawals_update_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\admin\\withdrawals-update.js"
+import { onRequestPost as __api_auth_forgot_password_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\auth\\forgot-password.js"
+import { onRequestPost as __api_auth_login_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\auth\\login.js"
+import { onRequestPost as __api_auth_logout_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\auth\\logout.js"
+import { onRequestGet as __api_auth_me_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\auth\\me.js"
+import { onRequestPost as __api_auth_register_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\auth\\register.js"
+import { onRequestPost as __api_auth_reset_password_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\auth\\reset-password.js"
+import { onRequestGet as __api_offers_admantum_feed_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\admantum-feed.js"
+import { onRequestGet as __api_offers_admantum_postback_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\admantum-postback.js"
+import { onRequestPost as __api_offers_admantum_postback_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\admantum-postback.js"
+import { onRequestGet as __api_offers_cpalead_postback_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\cpalead-postback.js"
+import { onRequestPost as __api_offers_cpalead_postback_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\cpalead-postback.js"
+import { onRequestGet as __api_offers_earnings_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\earnings.js"
+import { onRequestGet as __api_offers_gemiad_postback_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\gemiad-postback.js"
+import { onRequestPost as __api_offers_gemiad_postback_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\gemiad-postback.js"
+import { onRequestGet as __api_offers_offery_feed_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\offery-feed.js"
+import { onRequestGet as __api_offers_offery_postback_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\offery-postback.js"
+import { onRequestPost as __api_offers_offery_postback_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\offery-postback.js"
+import { onRequestGet as __api_offers_paidbucksy_postback_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\paidbucksy-postback.js"
+import { onRequestPost as __api_offers_paidbucksy_postback_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\paidbucksy-postback.js"
+import { onRequestGet as __api_offers_radientwall_postback_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\radientwall-postback.js"
+import { onRequestPost as __api_offers_radientwall_postback_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\radientwall-postback.js"
+import { onRequestGet as __api_offers_revtoo_feed_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\revtoo-feed.js"
+import { onRequestGet as __api_offers_revtoo_postback_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\revtoo-postback.js"
+import { onRequestPost as __api_offers_revtoo_postback_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\revtoo-postback.js"
+import { onRequestGet as __api_offers_taskwall_feed_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\taskwall-feed.js"
+import { onRequestGet as __api_offers_taskwall_postback_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\taskwall-postback.js"
+import { onRequestPost as __api_offers_taskwall_postback_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\taskwall-postback.js"
+import { onRequestGet as __api_offers_vortexwall_postback_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\vortexwall-postback.js"
+import { onRequestPost as __api_offers_vortexwall_postback_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\vortexwall-postback.js"
+import { onRequestPost as __api_profile_update_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\profile\\update.js"
+import { onRequestGet as __api_withdraw_request_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\withdraw\\request.js"
+import { onRequestPost as __api_withdraw_request_js_onRequestPost } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\withdraw\\request.js"
+import { onRequest as __api_offers_primewall_postback_js_onRequest } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\offers\\primewall-postback.js"
+import { onRequestGet as __api_activity_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\activity.js"
+import { onRequestGet as __api_leaderboard_js_onRequestGet } from "C:\\Users\\AC\\OneDrive\\Documents\\incm website\\functions\\api\\leaderboard.js"
+
+export const routes = [
+    {
+      routePath: "/api/admin/dashboard-stats",
+      mountPath: "/api/admin",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_admin_dashboard_stats_js_onRequestGet],
+    },
+  {
+      routePath: "/api/admin/users",
+      mountPath: "/api/admin",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_admin_users_js_onRequestGet],
+    },
+  {
+      routePath: "/api/admin/withdrawals",
+      mountPath: "/api/admin",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_admin_withdrawals_js_onRequestGet],
+    },
+  {
+      routePath: "/api/admin/withdrawals-update",
+      mountPath: "/api/admin",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_admin_withdrawals_update_js_onRequestPost],
+    },
+  {
+      routePath: "/api/auth/forgot-password",
+      mountPath: "/api/auth",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_auth_forgot_password_js_onRequestPost],
+    },
+  {
+      routePath: "/api/auth/login",
+      mountPath: "/api/auth",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_auth_login_js_onRequestPost],
+    },
+  {
+      routePath: "/api/auth/logout",
+      mountPath: "/api/auth",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_auth_logout_js_onRequestPost],
+    },
+  {
+      routePath: "/api/auth/me",
+      mountPath: "/api/auth",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_auth_me_js_onRequestGet],
+    },
+  {
+      routePath: "/api/auth/register",
+      mountPath: "/api/auth",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_auth_register_js_onRequestPost],
+    },
+  {
+      routePath: "/api/auth/reset-password",
+      mountPath: "/api/auth",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_auth_reset_password_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/admantum-feed",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_admantum_feed_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/admantum-postback",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_admantum_postback_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/admantum-postback",
+      mountPath: "/api/offers",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_offers_admantum_postback_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/cpalead-postback",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_cpalead_postback_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/cpalead-postback",
+      mountPath: "/api/offers",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_offers_cpalead_postback_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/earnings",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_earnings_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/gemiad-postback",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_gemiad_postback_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/gemiad-postback",
+      mountPath: "/api/offers",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_offers_gemiad_postback_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/offery-feed",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_offery_feed_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/offery-postback",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_offery_postback_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/offery-postback",
+      mountPath: "/api/offers",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_offers_offery_postback_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/paidbucksy-postback",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_paidbucksy_postback_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/paidbucksy-postback",
+      mountPath: "/api/offers",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_offers_paidbucksy_postback_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/radientwall-postback",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_radientwall_postback_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/radientwall-postback",
+      mountPath: "/api/offers",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_offers_radientwall_postback_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/revtoo-feed",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_revtoo_feed_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/revtoo-postback",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_revtoo_postback_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/revtoo-postback",
+      mountPath: "/api/offers",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_offers_revtoo_postback_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/taskwall-feed",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_taskwall_feed_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/taskwall-postback",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_taskwall_postback_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/taskwall-postback",
+      mountPath: "/api/offers",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_offers_taskwall_postback_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/vortexwall-postback",
+      mountPath: "/api/offers",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_offers_vortexwall_postback_js_onRequestGet],
+    },
+  {
+      routePath: "/api/offers/vortexwall-postback",
+      mountPath: "/api/offers",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_offers_vortexwall_postback_js_onRequestPost],
+    },
+  {
+      routePath: "/api/profile/update",
+      mountPath: "/api/profile",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_profile_update_js_onRequestPost],
+    },
+  {
+      routePath: "/api/withdraw/request",
+      mountPath: "/api/withdraw",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_withdraw_request_js_onRequestGet],
+    },
+  {
+      routePath: "/api/withdraw/request",
+      mountPath: "/api/withdraw",
+      method: "POST",
+      middlewares: [],
+      modules: [__api_withdraw_request_js_onRequestPost],
+    },
+  {
+      routePath: "/api/offers/primewall-postback",
+      mountPath: "/api/offers",
+      method: "",
+      middlewares: [],
+      modules: [__api_offers_primewall_postback_js_onRequest],
+    },
+  {
+      routePath: "/api/activity",
+      mountPath: "/api",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_activity_js_onRequestGet],
+    },
+  {
+      routePath: "/api/leaderboard",
+      mountPath: "/api",
+      method: "GET",
+      middlewares: [],
+      modules: [__api_leaderboard_js_onRequestGet],
+    },
+  ]
